8cd4ff7498179216d1fcbd91e75b1375b419a6e1
Windy Feng <1745500559@qq.com>
2019-10-25 18:53:45 -0400

    Fix Re-Install Bugs

--- Utilities/install_newest_ds_mac.sh
+++ Utilities/install_newest_ds_mac.sh
@@ -1,9 +1,15 @@
 echo "-----------------------------------------"
-echo "Installing Driver Station 5.3 On Phone..."
+echo "Uninstalling Previous Versions of DS"
+echo "-----------------------------------------"
+
+adb uninstall com.qualcomm.ftcdriverstation
+
+echo "-----------------------------------------"
+echo "Installing Driver Station 5.3 On Phone"
 echo "-----------------------------------------"
 
 adb install "../Driver-Station-APKs/FtcDriverStation-release-5.3.apk"
 
 echo "-----------------------------------------"
-echo "Finished Installing"
+echo "Installation Finished"
 echo "-----------------------------------------"
