6b1fa60e8216fc291d3a18400c0dd5ce29667ba8
Windy Feng <1745500559@qq.com>
2019-10-25 13:03:18 -0400

    Initial commit

---
+++ Utilities/install_newest_ds_mac.sh
@@ -0,0 +1,9 @@
+echo "-----------------------------------------"
+echo "Installing Driver Station 5.3 On Phone..."
+echo "-----------------------------------------"
+
+adb install "../Driver-Station-APKs/FtcDriverStation-release-5.3.apk"
+
+echo "-----------------------------------------"
+echo "Finished Installing"
+echo "-----------------------------------------"
---
+++ Utilities/install_newest_ds_windows.bat
@@ -0,0 +1,9 @@
+@echo off
+echo -----------------------------------------
+echo Installing Driver Station 5.3 On Phone...
+echo -----------------------------------------
+adb install "../Driver-Station-APKs/FtcDriverStation-release-5.3.apk"
+echo -----------------------------------------
+echo Finished Installing
+echo -----------------------------------------
+pause
